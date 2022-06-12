34245c15fe40ba28dd2dffd45dd3c74e8aac9b3e
Neil Suttie <nsuttie@ucreative.ac.uk>
2022-06-12 12:12:35 +0100

    NavMeshDemo Commit
    -Added the NanMesh Demo project. 
    -Last tested on Unity 2021.1.17f1

---
+++ NavMeshDemo/Assets/Scripts/NavMeshTarget.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshTarget : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+    public Transform targetMarker;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void UpdateTargets(Vector3 targetPosition)
+    {
+        navAgent.destination = targetPosition;
+    }
+
+    private void Update()
+    {
+        if (GetInput())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                UpdateTargets(targetPosition);
+                targetMarker.position = targetPosition;
+            }
+        }
+    }
+
+    private bool GetInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(targetMarker.position, targetMarker.position + Vector3.up * 5, Color.red);
+    }
+}
---
+++ NavMeshDemo/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator doorController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            doorController.SetTrigger("OpenOrClose");
+        }
+        
+    }
+}
